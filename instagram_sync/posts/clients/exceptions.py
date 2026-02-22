7f751860da869e4d05a7db2b00b11205a7e5315e
Mikita Aksianchuk <mikita.aksianchuk@gmail.com>
2026-02-22 20:36:36 +0300

    Add new exceptions

--- instagram_sync/posts/clients/exceptions.py
+++ instagram_sync/posts/clients/exceptions.py
@@ -19,3 +19,13 @@ class InstagramApiClientConnectionException(APIException):
 class InstagramApiClientTimeoutException(APIException):
     status_code = status.HTTP_503_SERVICE_UNAVAILABLE
     default_detail = "Превышен лимит на соединение или ответ от Instagram API"
+
+
+class InstagramApiClientPostNotFoundException(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = "Пост не найден"
+
+
+class InstagramApiClientInvalidAccessTokenException(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = "Невалидный Access Token"
